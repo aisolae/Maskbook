286e229623365a1f0b49779ebc852060d38a0742
SunriseFox <sunrisefox@qq.com>
2019-11-26 16:32:30 +0800

    feat: basic navigation

--- src/extension/options-page/Home.tsx
+++ src/extension/options-page/Home.tsx
@@ -123,7 +123,7 @@ export default function DashboardHomePage() {
                             <ListItemSecondaryAction>
                                 <Button
                                     component={Link}
-                                    to="/initialize"
+                                    to="database/backup"
                                     variant="contained"
                                     color="primary"
                                     className={classes.button}>
@@ -172,7 +172,12 @@ export default function DashboardHomePage() {
                         <ListItem key="persona-create">
                             <ListItemText primary="Create" secondary="Create a new persona." />
                             <ListItemSecondaryAction>
-                                <Button variant="contained" color="primary" className={classes.button}>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.button}
+                                    component={Link}
+                                    to="persona/create">
                                     Create
                                 </Button>
                             </ListItemSecondaryAction>
@@ -181,7 +186,12 @@ export default function DashboardHomePage() {
                         <ListItem key="persona-import">
                             <ListItemText primary="Import" secondary="From a previous persona backup." />
                             <ListItemSecondaryAction>
-                                <Button variant="contained" color="default" className={classes.button}>
+                                <Button
+                                    variant="contained"
+                                    color="default"
+                                    className={classes.button}
+                                    component={Link}
+                                    to="persona/import">
                                     Import
                                 </Button>
                             </ListItemSecondaryAction>
@@ -201,7 +211,12 @@ export default function DashboardHomePage() {
                                 secondary="Create a database backup file. Do it frequently."
                             />
                             <ListItemSecondaryAction>
-                                <Button variant="contained" color="primary" className={classes.button}>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.button}
+                                    component={Link}
+                                    to="database/backup">
                                     Backup
                                 </Button>
                             </ListItemSecondaryAction>
@@ -210,7 +225,12 @@ export default function DashboardHomePage() {
                         <ListItem key="dashboard-restore">
                             <ListItemText primary="Restore" secondary="From a previous database backup." />
                             <ListItemSecondaryAction>
-                                <Button variant="contained" color="default" className={classes.button}>
+                                <Button
+                                    variant="contained"
+                                    color="default"
+                                    className={classes.button}
+                                    component={Link}
+                                    to="database/restore">
                                     Restore
                                 </Button>
                             </ListItemSecondaryAction>
